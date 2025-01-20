a05ab8dc199ee56426b51afd70232a0a803271dc
yashwanth-cd <yashwanth.3270@gmail.com>
2025-01-20 14:14:31 +0530

    Updated cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello, world! This is your API.");
+  res.send("Hello, world! This is your Render API.");
 });
 
 app.get("/api/example", (req, res) => {
