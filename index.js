e6d8bb289d90a41ae68d6c8031fdb590fe37855f
yashwanth-cd <yashwanth.3270@gmail.com>
2025-01-20 13:51:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world! This is your API.");
+});
+
+app.get("/api/example", (req, res) => {
+  res.json({ message: "This is an example endpoint." });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
